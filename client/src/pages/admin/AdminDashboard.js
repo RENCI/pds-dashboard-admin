06a46f8ff4da4d4bc6491fdd84183024c56e9f4e
Ryan O'Connor <rocsteady888@gmail.com>
2020-02-04 15:46:47 -0500

    initial commit

--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -4,21 +4,13 @@ import Grid from "@material-ui/core/Grid";
 
 export default function AdminDashboard() {
 
-  return ( <
-    >
-    <
-    Grid container spacing = {
-      3
-    } >
-    <
-    Grid item xs = {
-      12
-    } >
-    <
-    AdminMenu / >
-    <
-    /Grid> <
-    /Grid> <
-    />
+  return (
+    <>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <AdminMenu />
+        </Grid>
+      </Grid>
+    </>
   );
 }
