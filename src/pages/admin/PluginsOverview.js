578fd2e93d22caed2bb868de2e8eb49e6561ccf5
Ryan O'Connor <rocsteady888@gmail.com>
2020-04-13 10:13:20 -0400

    Removed "Version" from table it is currently not in the config object

--- src/pages/admin/PluginsOverview.js
+++ src/pages/admin/PluginsOverview.js
@@ -4,26 +4,25 @@ import CustomTable from "../../components/custom-table/custom-table.component";
 
 const PluginsOverview = () => {
   const context = useContext(ConfigContext);
-  console.log(context)
   const { plugins } = context.state;
   return (
     <>
       <CustomTable
         title={"Mapping Plugins"}
-        tableHeaders={["Enabled", "ID", "Version", "Plugin Selectors", "Supported Variables", "Title"]}
-        pluginKeys={["enabled", "piid", "version", "pluginSelectors", "supportedPatientVariables", "title"]}
+        tableHeaders={["Enabled", "ID", "Plugin Selectors", "Supported Variables", "Title"]}
+        pluginKeys={["enabled", "piid", "pluginSelectors", "supportedPatientVariables", "title"]}
         plugins={plugins.filter(plugins => plugins.pluginType === 'm')}
       />
       <CustomTable
         title={"Guidance Plugins"}
-        tableHeaders={["Enabled", "ID", "Version", "Parameter Defaults", "Plugin Selectors", "Patient Variables", "Title"]}
-        pluginKeys={["enabled", "piid", "version", "pluginParameterDefaults", "pluginSelectors", "requiredPatientVariables", "title"]}
+        tableHeaders={["Enabled", "ID", "Parameter Defaults", "Plugin Selectors", "Patient Variables", "Title"]}
+        pluginKeys={["enabled", "piid", "pluginParameterDefaults", "pluginSelectors", "requiredPatientVariables", "title"]}
         plugins={plugins.filter(plugins => plugins.pluginType === 'g')}
       />
       <CustomTable
         title={"FHIR Plugins"}
-        tableHeaders={["Enabled", "ID", "Version", "Plugin Selectors", "Title"]}
-        pluginKeys={["enabled", "piid", "version", "pluginSelectors", "title"]}
+        tableHeaders={["Enabled", "ID", "Plugin Selectors", "Title"]}
+        pluginKeys={["enabled", "piid", "pluginSelectors", "title"]}
         plugins={plugins.filter(plugins => plugins.pluginType === 'f')}
       />
     </>
