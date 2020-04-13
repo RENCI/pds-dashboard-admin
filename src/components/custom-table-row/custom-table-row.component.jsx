5b5bc23d44584de68d0c54e6205d80a368a4680d
Ryan O'Connor <rocsteady888@gmail.com>
2020-04-13 10:12:48 -0400

    added dispatch to toggle enabled

--- src/components/custom-table-row/custom-table-row.component.jsx
+++ src/components/custom-table-row/custom-table-row.component.jsx
@@ -8,11 +8,12 @@ import {
 } from "@material-ui/core";
 
 const CustomTableRow = ({ plugin }) => {
-  const context = useContext(ConfigContext);
+  const {dispatch} = useContext(ConfigContext);
   const [enabled, setEnabled] = useState(plugin.enabled);
 
   const handleChange = () => {
     setEnabled(!enabled)
+    dispatch({ type: "TOGGLE_ENABLED", payload: {piid: plugin.piid, enabled: !enabled} })
     console.table(plugin)
   }
   return (
@@ -26,7 +27,6 @@ const CustomTableRow = ({ plugin }) => {
         />
       </TableCell>
       <TableCell>{plugin.piid}</TableCell>
-      <TableCell>{plugin.version}</TableCell>
       {plugin.pluginParameterDefaults ? <TableCell>{plugin.pluginParameterDefaults[0].parameterValue.value}</TableCell> : null}
       {plugin.pluginSelectors ? <TableCell>{plugin.pluginSelectors.filter(selector => selector.selectorValue).map(item => `${item.selectorValue.title} : ${item.selectorValue.value}`)}</TableCell> : null}
       {plugin.requiredPatientVariables ? <TableCell>{`${plugin.requiredPatientVariables[0].id} and ${plugin.requiredPatientVariables.length} others`}</TableCell> : null}
