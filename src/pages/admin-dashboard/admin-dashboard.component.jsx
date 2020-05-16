deabbe500e114e5da6963057ce9a7800d09c8e7a
Ryan O'Connor <rocsteady888@gmail.com>
2020-05-16 16:37:31 -0400

    added toggle for config and example

--- src/pages/admin-dashboard/admin-dashboard.component.jsx
+++ src/pages/admin-dashboard/admin-dashboard.component.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { ConfigContext } from '../../context/config-context';
 import CustomTable from '../../components/custom-table/custom-table.component';
 import CustomTableSwitch from '../../components/custom-table-switch/custom-table-switch.component';
@@ -7,20 +7,33 @@ import './admin-dashboard.styles.scss';
 
 const AdminDashboard = () => {
   const context = useContext(ConfigContext);
-  const { plugins } = context.state;
+  const { plugins, examplePlugins } = context.state;
+  const [ useExampleData, setUseExampleData ] = useState(false);
+
+  const toggleDataSource = () => {
+    setUseExampleData(!useExampleData)
+  }
+  
   return (
     <div className="container">
       <div className="grid-item">
         <h1 className="title">PDS Admin Dashboard</h1>
+        <div
+          className={useExampleData ? "data-source-selection example-data" : "data-source-selection config-data"} 
+          onClick={toggleDataSource}
+        >
+          {useExampleData ? "Load Config Data" : "Load Example Data"}
+        </div>
       </div>
       <CustomTable
         title={"Mapping Plugins"}
         tableHeaders={[
           { title: "Enabled", field: 'enabled', render: rowData => <CustomTableSwitch enabled={rowData.enabled} piid={rowData.piid} /> },
-          { title: "ID", field: "piid" },
+          { title: "ID", field: "piid", render: rowData => rowData.piid},
           { title: "Title", field: "title" }
         ]}
-        plugins={plugins.filter(plugins => plugins.pluginType === 'm')}
+        plugins={useExampleData ? examplePlugins.filter(examplePlugins => examplePlugins.pluginType === 'm') : plugins.filter(plugins => plugins.pluginType === 'm')}
+        defaultPlugin={plugins.filter(plugins => plugins.pluginType === 'md')}
       />
       <CustomTable
         title={"Guidance Plugins"}
@@ -29,7 +42,16 @@ const AdminDashboard = () => {
           { title: "ID", field: "piid" },
           { title: "Title", field: "title" }
         ]}
-        plugins={plugins.filter(plugins => plugins.pluginType === 'g')}
+        plugins={useExampleData ? examplePlugins.filter(examplePlugins => examplePlugins.pluginType === 'g') : plugins.filter(plugins => plugins.pluginType === 'g')}
+      />
+      <CustomTable
+        title={"Convenience Plugins"}
+        tableHeaders={[
+          { title: "Enabled", field: 'enabled', render: rowData => <CustomTableSwitch enabled={rowData.enabled} piid={rowData.piid} /> },
+          { title: "ID", field: "piid" },
+          { title: "Title", field: "title" }
+        ]}
+        plugins={useExampleData ? examplePlugins.filter(examplePlugins => examplePlugins.pluginType === 'c') : plugins.filter(plugins => plugins.pluginType === 'c')}
       />
       <CustomTable
         title={"FHIR Plugins"}
@@ -38,27 +60,10 @@ const AdminDashboard = () => {
           { title: "ID", field: "piid" },
           { title: "Title", field: "title" }
         ]}
-        plugins={plugins.filter(plugins => plugins.pluginType === 'f')}
+        plugins={useExampleData ? examplePlugins.filter(examplePlugins => examplePlugins.pluginType === 'f') : plugins.filter(plugins => plugins.pluginType === 'f')}
+        defaultPlugin={plugins.filter(plugins => plugins.pluginType === 'fd')}
       />
     </div>
-    /* <CustomTable
-      title={"Mapping Plugins"}
-      tableHeaders={["Enabled", "ID", "Plugin Selectors", "Supported Variables", "Title"]}
-      pluginKeys={["enabled", "piid", "pluginSelectors", "supportedPatientVariables", "title"]}
-      plugins={plugins.filter(plugins => plugins.pluginType === 'm')}
-    />
-    <CustomTable
-      title={"Guidance Plugins"}
-      tableHeaders={["Enabled", "ID", "Parameter Defaults", "Plugin Selectors", "Patient Variables", "Title"]}
-      pluginKeys={["enabled", "piid", "pluginParameterDefaults", "pluginSelectors", "requiredPatientVariables", "title"]}
-      plugins={plugins.filter(plugins => plugins.pluginType === 'g')}
-    />
-    <CustomTable
-      title={"FHIR Plugins"}
-      tableHeaders={["Enabled", "ID", "Plugin Selectors", "Title"]}
-      pluginKeys={["enabled", "piid", "pluginSelectors", "title"]}
-      plugins={plugins.filter(plugins => plugins.pluginType === 'f')}
-    /> */
   );
 }
 
