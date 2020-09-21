5bfc8e3a0708ecadfb945a9e20a46580e840731d
David <borland@renci.org>
2020-09-21 16:31:50 -0400

    Add selector title, experiment with material ui for styling

--- src/components/plugin-details/plugin-details.component.jsx
+++ src/components/plugin-details/plugin-details.component.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
+import { Box } from '@material-ui/core';
 
 import './plugin-details.styles.scss';
  
 const PluginDetails = ({ selectors }) => {
   return (
-    <div style={{ marginLeft: "10px" }}>
+    <Box ml={2}>
       <h4>Plugin Selectors</h4>
       <ul>
         { selectors.map(selector => (
             <li key={selector.selectorValue.title}>
-              {selector.selectorValue.value}, {selector.selectorValue.title}
+              <Box component='span' fontWeight='fontWeightBold' mr={2}>{ selector.title }</Box>
+              <Box component='span' mr={2}>{ selector.selectorValue.value }</Box>
+              <Box component='span' fontStyle='italic'>{ selector.selectorValue.title }</Box>
             </li> 
           )
         )}
       </ul>
-    </div>
+    </Box>
   )
 }
 
