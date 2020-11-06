7215aa5d32011187f7671c8bb7a40a4e8f4746a9
David <borland@renci.org>
2020-11-06 15:30:45 -0500

    Add selector display

--- src/components/selector-table-selectors/selector-table-selectors.component.jsx
+++ src/components/selector-table-selectors/selector-table-selectors.component.jsx
@@ -1,20 +1,44 @@
 import React from "react";
-import { 
-  Box,
-  MenuItem,  
-  FormControl, 
-  Select } from "@material-ui/core";
+import { Box } from "@material-ui/core";
+
+const typeMap = {
+  g: "Guidance",
+  c: "Convenience",
+  m: "Mapper",
+  f: "FHIR",
+  mD: "Default Mapper",
+  fD: "Default FHIR"
+};
 
 const SelectorTableSelectors = ({ selectors }) => {
-  //const classes = useStyles();
+  const filterPluginType = true;
 
-  const handleChange = (event) => {
-    console.log(event.target.value);
-  };   
+  const selectorDisplay = selector => (
+    <>
+      <Box component="span" fontStyle="italic">{ selector.id }</Box>
+      —<Box component="span">{ selector.title }</Box>
+      : <Box component="span" fontWeight="fontWeightMedium">{ selector.selectorValue.title }</Box>
+    </>
+  );
+
+  const pluginTypeDisplay = selector => (
+    <>
+      <Box component="span" fontStyle="italic">{ selector.id }</Box>
+      : <Box component="span" fontWeight="fontWeightMedium">{ typeMap[selector.selectorValue.value] }</Box>
+    </>
+  );
 
   return (
     <>
-      Hello
+      { selectors
+          .filter(selector => filterPluginType ? 
+            !(selector.id === "pluginType" && selector.selectorValue.value === "g") : true
+          ).map((selector, i, a) => (
+            <div key={ i }>
+              { selector.id === "pluginType" ? pluginTypeDisplay(selector) : selectorDisplay(selector) }
+              { i !== a.length - 1 ? <Box ml={ 1 }>AND</Box> : null }
+            </div>
+          ))}
     </>
   )
 };
