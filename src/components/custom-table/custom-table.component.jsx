a1cdc4cb4a73bf7d2b498a0aca72e94b91f6c6ad
David <borland@renci.org>
2020-12-04 16:47:08 -0500

    Remove unused variables

--- src/components/custom-table/custom-table.component.jsx
+++ src/components/custom-table/custom-table.component.jsx
@@ -1,13 +1,10 @@
 import React, { useState } from 'react';
 import MaterialTable, { MTableToolbar } from 'material-table';
-import SelectorList from '../selector-list/selector-list.component';
 import PluginDetails from '../plugin-details/plugin-details.component';
 
 import './custom-table.styles.scss';
 
 const CustomTable = ({ plugins, title, tableHeaders }) => {
-  const [filterSelectorList, setFilterSelectedList] = useState(['ICD-10CM:I48.91']);
-
   return (
     <div className="grid-item">
       <MaterialTable
